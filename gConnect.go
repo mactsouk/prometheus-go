2eb105e4fbaa426564872fc5f408da98cfaa5336
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-08-13 17:48:40 +0300

    Correcting to Gmail + number of emails

--- gConnect.go
+++ gConnect.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// PORT is the TCP port number the server will listen to
+var PORT = ":1234"
+
+var (
+	counter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "custom",
+			Name:      "my_counter",
+			Help:      "# of emails in the Gmail account",
+		})
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	http.Handle("/metrics", promhttp.Handler())
+	prometheus.MustRegister(counter)
+
+	go func() {
+		for {
+			counter.Add(rand.Float64() * 5)
+
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	log.Println("Listening to port", PORT)
+	log.Println(http.ListenAndServe(PORT, nil))
 }
