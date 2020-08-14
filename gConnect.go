2bd6eb009746a047f4790d3fd483df10b412287d
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-08-14 21:28:49 +0300

    Working after following Google instructions

--- gConnect.go
+++ gConnect.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
@@ -10,8 +13,9 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/api/gmail/v1"
-	"google.golang.org/api/option"
 )
 
 // PORT is the TCP port number the server will listen to
@@ -26,26 +30,101 @@ var (
 		})
 )
 
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
 func main() {
+	b, err := ioutil.ReadFile("./credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
 	rand.Seed(time.Now().Unix())
 	http.Handle("/metrics", promhttp.Handler())
 	prometheus.MustRegister(counter)
 
-	APIKey := os.Getenv("GMAIL")
-	ctx := context.Background()
-	gmailService, err := gmail.NewService(ctx, option.WithAPIKey(APIKey))
+	gmailService, err := gmail.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Gmail client: %v", err)
+	}
 
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	log.Printf("%+v\n", gmailService)
+	// fmt.Printf("%T\n", msgs.Messages)
+	// for i, v := range msgs.Messages {
+	//		fmt.Println(i, v.ThreadId)
+	//	}
 
 	go func() {
 		for {
-			counter.Add(rand.Float64() * 5)
-
+			msgs, err := gmailService.Users.Messages.List("me").Do()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			fmt.Println("Number of messages:", len(msgs.Messages))
 			time.Sleep(5 * time.Second)
 		}
 	}()
